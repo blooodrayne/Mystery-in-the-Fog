3aded0635d745c2e3bab227e81fac0e34da89b92
DESKTOP-ALNIJ4A\Полина <pollisuperkahnum@gmail.com>
2022-05-20 03:18:11 +0300

    Add backs, characters, views, experiment with fade and view

---
+++ Assets/InkFungus/Scripts/StoryStateWrapper.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using Ink.Runtime;
+
+namespace InkFungus
+{
+    // This is a solution to the problem of finding the cPath for nodes with choices
+    // and final nodes (END/DONE).
+
+    class StoryStateWrapper
+    {
+        public readonly string cPath;
+
+        public StoryStateWrapper(StoryState state)
+        {                        
+            cPath = state.currentPathString;
+            if (cPath == null && state.currentChoices.Count > 0) {
+                cPath =  state.currentChoices[0].targetPath.ToString();
+            }
+            if (cPath == null) // Again! Means that this is END/DONE
+            {
+                cPath = state.previousPointer.path.ToString();
+            }
+        }
+    }
+}
