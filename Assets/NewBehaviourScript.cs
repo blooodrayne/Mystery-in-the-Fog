19bbe497237bd4ceca1ad8ba4021be01730446dd
DESKTOP-ALNIJ4A\Полина <pollisuperkahnum@gmail.com>
2022-08-15 19:21:49 +0300

    add new

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Fungus;
 
 public class NewBehaviourScript : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        Flowchart flowchart = new Flowchart();
+        flowchart.LocalizationId.Insert(1, PlayerPrefs.GetString())
     }
 
     // Update is called once per frame
